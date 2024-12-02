018e6eb70de0328ee1884201bab87bca0d854ed3
Witty-Wizard <agarwalshashank429+github@gmail.com>
2024-12-02 16:57:30 +0530

    Added: Build Command for packaging the electron application

--- src/main/main.js
+++ src/main/main.js
@@ -1,17 +1,25 @@
-import { BrowserWindow, ipcMain, app } from "electron";
-import { spawn } from "child_process";
+import { BrowserWindow, app } from "electron";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true,
+      preload: path.join(__dirname, "../renderer/preload.js"),
+      contextIsolation: true,
     },
   });
 
-  // Load the Vite development server's URL
-  win.loadURL("http://localhost:5173");
+  if (process.env.NODE_ENV === "development") {
+    win.loadURL("http://localhost:5173");
+  } else {
+    win.loadFile(path.join(__dirname,"../../build/renderer/index.html"));
+  }
 };
 
 app.whenReady().then(() => {
